ff94ee61693bb892348a098b48984c2f67da0328
dukesrg <dukesrg@gmail.com>
2020-05-03 12:51:21 +0300

    Added missing project files

---
+++ Supersaw-osc/project.mk
@@ -0,0 +1,19 @@
+# #############################################################################
+# Project Customization
+# #############################################################################
+
+include $(PLATFORMDIR)/osc.mk
+
+PROJECT = Supersaw
+
+UCSRC =
+
+UCXXSRC = ../src/supersaw.cpp
+
+UINCDIR =
+
+UDEFS =
+
+ULIB =
+
+ULIBDIR =
