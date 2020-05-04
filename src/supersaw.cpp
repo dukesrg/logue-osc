d29360d128b6b860b0d32569236bae4ae2a4c73e
dukesrg <dukesrg@gmail.com>
2020-05-04 14:08:46 +0300

    Optimized voice allocation

--- src/supersaw.cpp
+++ src/supersaw.cpp
@@ -13,6 +13,7 @@
 #define MAX_UNISON 12 //maximum unison pairs
 #define MAX_DETUNE 1.f //maximum detune between neighbor unison voices in semitones
 #define MAX_POLY 12 //maximum polyphony
+#define NOPITCH 0xFFFF
 
 static float s_unison;
 static float s_detune;
@@ -20,6 +21,7 @@ static float s_amp;
 static uint32_t s_max_unison;
 static float s_max_detune;
 static uint32_t s_max_poly;
+static uint32_t s_voice_count;
 static uint32_t s_voice_index;
 static uint32_t s_lfo_route;
 static float s_shape;
@@ -40,14 +42,14 @@ void OSC_INIT(__attribute__((unused)) uint32_t platform, __attribute__((unused))
   s_max_unison = MAX_UNISON;
   s_max_detune = MAX_DETUNE;
   s_max_poly = 1;
+  s_voice_count = 0;
   s_voice_index = 0;
   s_lfo_route = 1;
-  for (uint32_t i = MAX_POLY; i--; s_pitch[i] = 0xFFFF);
   s_shape = 0.f;
   s_shiftshape = 0.f;
-  s_note_pitch = 0xFFFF;
-  s_old_pitch = 0xFFFF;
-  s_osc_pitch = 0xFFFF;
+  s_note_pitch = NOPITCH;
+  s_old_pitch = NOPITCH;
+  s_osc_pitch = NOPITCH;
 }
 
 __fast_inline float osc_w0f_for_notef(uint8_t note, float mod) {
@@ -60,8 +62,8 @@ void OSC_CYCLE(const user_osc_param_t * const params, int32_t *yn, const uint32_
   if (s_note_pitch != s_old_pitch || params->pitch != s_osc_pitch) {
     uint16_t noteoff = params->pitch - s_osc_pitch + s_old_pitch;
     s_old_pitch = s_note_pitch;
-    for (i = 0; i < s_max_poly && s_pitch[i] != noteoff; i++);
-    if (i >= s_max_poly) {
+    for (i = 0; i < s_voice_count && s_pitch[i] != noteoff; i++);
+    if (i >= s_voice_count) {
       s_osc_pitch = params->pitch;
       s_pitch_wheel = params->pitch - s_note_pitch;
     } else {
@@ -82,7 +84,7 @@ void OSC_CYCLE(const user_osc_param_t * const params, int32_t *yn, const uint32_
   if (s_lfo_route & 0x2)
     detune *= lfo;
 
-  for (j = 0; s_pitch[j] != 0xFFFF && j < s_max_poly; j++) {
+  for (j = s_voice_count; j--;) {
     uint16_t pitch = s_pitch[j] + s_pitch_wheel;
     uint8_t note = pitch >> 8;
     uint8_t mod = pitch & 0xFF;
@@ -103,7 +105,7 @@ void OSC_CYCLE(const user_osc_param_t * const params, int32_t *yn, const uint32_
   q31_t * __restrict y = (q31_t *)yn;
   for (uint32_t f = frames; f--; y++) {
     float valf = .0f;
-    for (j = 0; s_pitch[j] != 0xFFFF && j < s_max_poly; j++) {
+    for (j = s_voice_count; j--;) {
       float *phase = s_phase[j];
       float *w0 = s_w0[j];
       for (i = 0; i <= base; i++, phase++, w0++) {
@@ -125,7 +127,7 @@ void OSC_CYCLE(const user_osc_param_t * const params, int32_t *yn, const uint32_
   }
 
   base += frac == 0.f ? 1 : 3;
-  for (j = 0; s_pitch[j] != 0xFFFF && j < s_max_poly; j++) {
+  for (j = s_voice_count; j--;) {
     float *phase = &s_phase[j][base];
     float *w0 = &s_w0[j][base];
     for (i = base; i <= MAX_UNISON * 2; i++, phase++, w0++) {
@@ -139,10 +141,12 @@ void OSC_NOTEON(const user_osc_param_t * const params)
 {
   s_note_pitch = params->pitch;
   uint32_t i;
-  for (i = 0; i < s_max_poly && s_pitch[i] != s_note_pitch; i++);
-  if (i >= s_max_poly) {
+  for (i = 0; i < s_voice_count && s_pitch[i] != s_note_pitch; i++);
+  if (i >= s_voice_count) {
     s_pitch[s_voice_index] = s_note_pitch;
     for (i = MAX_UNISON * 2 + 1; i--; s_phase[s_voice_index][i] = _osc_white());
+    if (s_voice_count < s_max_poly)
+      s_voice_count++;
     if (++s_voice_index >= s_max_poly)
       s_voice_index = 0;
   }
@@ -150,11 +154,11 @@ void OSC_NOTEON(const user_osc_param_t * const params)
 
 void OSC_NOTEOFF(__attribute__((unused)) const user_osc_param_t * const params)
 {
-  s_note_pitch = 0xFFFF;
-  s_osc_pitch = 0xFFFF;
-  s_old_pitch = 0xFFFF;
+  s_note_pitch = NOPITCH;
+  s_osc_pitch = NOPITCH;
+  s_old_pitch = NOPITCH;
+  s_voice_count = 0;
   s_voice_index = 0;
-  for (uint32_t i = MAX_POLY; i--; s_pitch[i] = 0xFFFF);
 }
 
 void OSC_PARAM(uint16_t index, uint16_t value)
