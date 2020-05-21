68c87f994fc78ee6c085633fd75d4ba50f533802
dukesrg <dukesrg@gmail.com>
2020-05-21 18:53:08 +0300

    Morpheus sctipt is a bit more user-friendly

--- Morpheus.sh
+++ Morpheus.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-unzip $1 Morpheus/payload.bin
-dd conv=notrunc bs=1 if=$2 of=Morpheus/payload.bin seek=64 count=16384
-zip -mr $1 Morpheus
+([ "$#" -ne 2 ] || [ ! -f $1 ] || [ ! -f $2 ]) && echo "Usage: ${0##*/} <logue oscillator file> <u-law compressed WAV or RAW file>" && exit
+SKIP=0
+# RIFF WAVE mono u-law
+[ "46464952 45564157 00010007" = "$(od -An -tx4 -N24 -v $2 | tr -d '\n' | tr -s ' ' | cut -d' ' -f2,4,7)" ] && SKIP=58
+unzip $1
+NAME=${2##*/}
+NAME=${NAME%.*}
+NAME=${NAME:0:12}
+sed -E s'/^( *"name" *: *")[^"]+(.*)$/\1'$NAME'\2/' Morpheus/manifest.json > Morpheus/manifest.new
+mv -f Morpheus/manifest.new Morpheus/manifest.json
+dd conv=notrunc bs=1 if=$2 skip=$SKIP of=Morpheus/payload.bin seek=64 count=16384
+zip -mr "${2%.*}.${1##*.}" Morpheus
