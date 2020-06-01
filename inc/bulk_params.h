3299f3b9e21aa5572608f5ce23f3ff46349c48d4
dukesrg <dukesrg@gmail.com>
2020-06-01 08:43:51 +0300

    Bulk params prg_id check added

--- inc/bulk_params.h
+++ inc/bulk_params.h
@@ -12,9 +12,9 @@
 
 #include "userosc.h"
 
-#define BULK_ID 0x3FFF
-#ifndef BULK_ID
-  #error "BULK_ID is not defined"
+#define BULK_PRG_ID 0x7F7F7F7F
+#ifndef BULK_PRG_ID
+  #error "BULK_PRG_ID is not defined"
 #endif
 
 #define BULK_PARAM_COUNT 6
@@ -37,28 +37,28 @@ uint16_t get_bulk_param(uint16_t param) {
 
 static inline __attribute__((always_inline, optimize("Ofast")))
 void bulk_receive(const user_osc_param_t * const params) {
-  static const uint16_t bulk_guard[5] = {0x0000, 0x3FFF, BULK_ID, 0x3FFF, 0x0000}; //Start/stop dump
+  static const uint16_t bulk_guard[] = {0x0000, 0x7F7F, BULK_PRG_ID & 0x7F7F, (BULK_PRG_ID >> 16) & 0x7F7F, 0x7F7F, 0x0000}; //Start/stop dump
   static uint16_t ccval, ccval_old;
   static uint8_t state;
 
   uint16_t cc42 = params->cutoff >> 6;
   uint16_t cc43 = params->resonance >> 6;
-  ccval = cc42 & (cc43 << 7);
+  ccval = cc42 & (cc43 << 8);
 
   if (ccval == ccval_old)
     return;
   switch(state) {
-    case 3:
+    case sizeof(bulk_guard)/sizeof(bulk_guard[0]) - 2:
       if (ccval == bulk_guard[state])
         state++;
       else
         set_bulk_param(cc42, cc43);
       break;
-    case 4:
+    case sizeof(bulk_guard)/sizeof(bulk_guard[0]) - 1:
       if (ccval == bulk_guard[state])
         state = 0;
       else {
-        set_bulk_param(ccval_old & 0x7F, ccval_old >> 7);
+        set_bulk_param(ccval_old & 0x7F, ccval_old >> 8);
         set_bulk_param(cc42, cc43);
         state--;
       }
@@ -72,6 +72,7 @@ void bulk_receive(const user_osc_param_t * const params) {
   }
   ccval_old = ccval;
 }
+
 /*
 void bulk_receive(uint16_t index, uint16_t value) {
 
