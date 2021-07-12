434adbcde19a9c18a0b33f2fb54bd5c33dfc7f82
dukesrg <dukesrg@gmail.com>
2021-07-12 20:17:45 +0300

    Different Morpheus internal encodings added

---
+++ Morpheus-P-osc/project.mk
@@ -0,0 +1,19 @@
+# #############################################################################
+# Project Customization
+# #############################################################################
+
+include $(PLATFORMDIR)/osc.mk
+
+PROJECT = Morpheus-P
+
+UCSRC =
+
+UCXXSRC = ../src/morpheus.cpp
+
+UINCDIR = $(PROJECTDIR)/../inc
+
+UDEFS = -DFORMAT_PCM8
+
+ULIBS =
+
+ULIBDIR =
