bb58e4247f521fbd3435eae7c8b732da5dfcc9f7
Oleg Burdaev <dukesrg@gmail.com>
2022-10-10 18:38:36 +0400

    Chordus chords added

--- src/chordus.cpp
+++ src/chordus.cpp
@@ -1,22 +1,23 @@
 /*
- * File: morpheus.cpp
+ * File: chordus.cpp
  *
- * Morphing wavetable oscillator
- * 
- * 2020-2021 (c) Oleg Burdaev
+ * Chordus oscillator
+ *
+ * 2022 (c) Oleg Burdaev
  * mailto: dukesrg@gmail.com
  *
  */
 
 #include "userosc.h"
 
-#define MAXPOLY 20
+#include "chordus.h"
 
-static uint32_t s_poly = 1;
-static float s_poly_recip = 1.f;
+static uint32_t s_chord = 0;
+static uint32_t s_chord_notes = 1;
+static float s_chord_notes_recip = 1.f;
+static uint16_t s_detunes[MAX_CHORD_NOTES] = {0};
 static float s_detune = 0.f;
-static uint16_t s_detunes[MAXPOLY] = {0};
-static float s_phase[MAXPOLY] = {0.f};
+static float s_phase[MAX_CHORD_NOTES] = {0.f};
 
 void OSC_INIT(__attribute__((unused)) uint32_t platform, __attribute__((unused)) uint32_t api)
 {
@@ -25,30 +26,28 @@ void OSC_INIT(__attribute__((unused)) uint32_t platform, __attribute__((unused))
 
 void OSC_CYCLE(const user_osc_param_t * const params, int32_t *yn, const uint32_t frames)
 {
- 
-  float w0[MAXPOLY];
+  float w0[MAX_CHORD_NOTES];
   uint16_t pitch;
-  for (uint32_t i = 0; i < s_poly; i++) {
+  for (uint32_t i = 0; i < s_chord_notes; i++) {
     pitch = params->pitch + s_detunes[i];
-    w0[i] = osc_w0f_for_note((pitch  >> 8) + i, pitch & 0xFF);
+    w0[i] = osc_w0f_for_note((pitch >> 8) + chords[s_chord][i], pitch & 0xFF);
   }
 
   q31_t * __restrict y = (q31_t *)yn;
   for (uint32_t f = frames; f--; y++) {
     float a = 0.f;
-    for (uint32_t i = 0; i < s_poly; i++) {
-        a += osc_sinf(s_phase[i]);
+    for (uint32_t i = 0; i < s_chord_notes; i++) {
+        a += osc_sinf(s_phase[i]) * s_chord_notes_recip;
         s_phase[i] += w0[i];
         s_phase[i] -= (uint32_t)(s_phase[i]);
     }
-//    *y = f32_to_q31(a * s_poly_recip);
     *y = f32_to_q31(a);
   }
 }
 
 void OSC_NOTEON(__attribute__((unused)) const user_osc_param_t * const params)
 {
-  for (uint32_t i = 0; i < MAXPOLY; i++) {
+  for (uint32_t i = 0; i < MAX_CHORD_NOTES; i++) {
     s_phase[i] = 0.f;
     if (i > 0) {
       s_detunes[i] = (uint16_t)(osc_rand() * s_detune) - 255;
@@ -63,8 +62,9 @@ void OSC_PARAM(uint16_t index, uint16_t value)
     case k_user_osc_param_shiftshape:
       break;
     case k_user_osc_param_id1:
-      s_poly = value;
-      s_poly_recip = 1.f / s_poly;
+      s_chord = value;
+      for (s_chord_notes = 1; s_chord_notes < MAX_CHORD_NOTES && chords[s_chord][s_chord_notes] != 0xFF; s_chord_notes++);
+      s_chord_notes_recip = 1.f / s_chord_notes;
       break;
     case k_user_osc_param_id2:
 //      s_detune = value * 5.96046e-10f; // (1/100) >> 24
